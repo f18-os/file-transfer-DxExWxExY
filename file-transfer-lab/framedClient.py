8366d7cccdbd7f3c48fae7bb6aaba99348505cb5
DxExWxExY <DxExWxExY@gmail.com>
2018-10-01 19:42:58 -0600

    Can send files to server

--- file-transfer-lab/framedClient.py
+++ file-transfer-lab/framedClient.py
@@ -1,67 +1,75 @@
 #! /usr/bin/env python3
 
 # Echo client program
-import socket, sys, re
-
-sys.path.append("../lib")       # for params
+import socket, sys, re, os
+sys.path.append("../lib")
 import params
-
 from framedSock import framedSend, framedReceive
 
+def init_client():
+    global s, debug
+    progname = "Client"
+    switchesVarDefaults = (
+        (('-s', '--server'), 'server', "127.0.0.1:50001"),
+        (('-d', '--debug'), "debug", False),
+        (('-?', '--usage'), "usage", False))
+    paramMap = params.parseParams(switchesVarDefaults)
+    server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
 
-switchesVarDefaults = (
-    (('-s', '--server'), 'server', "127.0.0.1:50001"),
-    (('-d', '--debug'), "debug", False), # boolean (set if present)
-    (('-?', '--usage'), "usage", False), # boolean (set if present)
-    )
-
-
-progname = "framedClient"
-paramMap = params.parseParams(switchesVarDefaults)
-
-server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
-
-if usage:
-    params.usage()
-
-
-try:
-    serverHost, serverPort = re.split(":", server)
-    serverPort = int(serverPort)
-except:
-    print("Can't parse server:port from '%s'" % server)
-    sys.exit(1)
-
-s = None
-for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
-    af, socktype, proto, canonname, sa = res
+    if usage:
+        params.usage()
     try:
-        print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
-        s = socket.socket(af, socktype, proto)
-    except socket.error as msg:
-        print(" error: %s" % msg)
-        s = None
-        continue
-    try:
-        print(" attempting to connect to %s" % repr(sa))
-        s.connect(sa)
-    except socket.error as msg:
-        print(" error: %s" % msg)
-        s.close()
-        s = None
-        continue
-    break
+        serverHost, serverPort = re.split(":", server)
+        serverPort = int(serverPort)
+    except:
+        print("Can't parse server:port from '%s'" % server)
+        sys.exit(1)
 
-if s is None:
-    print('could not open socket')
-    sys.exit(1)
+    s = None
+    for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+        af, socktype, proto, canonname, sa = res
+        try:
+            print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
+            s = socket.socket(af, socktype, proto)
+        except socket.error as msg:
+            print(" error: %s" % msg)
+            s = None
+            continue
+        try:
+            print(" attempting to connect to %s" % repr(sa))
+            s.connect(sa)
+        except socket.error as msg:
+            print(" error: %s" % msg)
+            s.close()
+            s = None
+            continue
+        break
+    if s is None:
+        print('could not open socket')
+        sys.exit(1)
 
+def send_message(user_input):
+    if re.match("put\s[\w\W]+", user_input):
+        trash, file = user_input.split(" ",1)
+        msg = "put " + file.rsplit('/', 1)[-1]
+        framedSend(s, msg.encode())
+        framedSend(s, open(file, "rb").read(), 1)
+    elif re.match("get\s[\w\W]+", user_input):
+        pass
+    elif user_input == "quit":
+        print("Killing Server...")
+        framedSend(s, "quit".encode())
+        print("Response:", framedReceive(s))
+        sys.exit(0)
+    else:
+        framedSend(s, user_input.encode())
+        print("Response:", framedReceive(s))
 
-print("sending hello world")
-framedSend(s, b"hello world", debug)
-print("received:", framedReceive(s, debug))
+def get_file():
+    pass
 
-print("sending hello world")
-framedSend(s, b"hello world", debug)
-print("received:", framedReceive(s, debug))
-
+if __name__ == '__main__':
+    init_client()
+    while True:
+        user_input = input("Enter Message\n> ")
+        send_message(user_input)
--- file-transfer-lab/framedServer.py
+++ file-transfer-lab/framedServer.py
@@ -1,39 +1,50 @@
 #! /usr/bin/env python3
 
+import sys, re, socket, os
 sys.path.append("../lib")       # for params
-import sys, re, socket, params
-
-switchesVarDefaults = (
-    (('-l', '--listenPort') ,'listenPort', 50001),
-    (('-d', '--debug'), "debug", False), # boolean (set if present)
-    (('-?', '--usage'), "usage", False), # boolean (set if present)
-    )
-
-progname = "echoserver"
-paramMap = params.parseParams(switchesVarDefaults)
-
-debug, listenPort = paramMap['debug'], paramMap['listenPort']
-
-if paramMap['usage']:
-    params.usage()
-
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
-bindAddr = ("127.0.0.1", listenPort)
-lsock.bind(bindAddr)
-lsock.listen(5)
-print("listening on:", bindAddr)
-
-sock, addr = lsock.accept()
-
-print("connection rec'd from", addr)
-
-
+import params
 from framedSock import framedSend, framedReceive
 
-while True:
-    payload = framedReceive(sock, debug)
-    if debug: print("rec'd: ", payload)
-    if not payload:
-        break
-    payload += b"!"             # make emphatic!
-    framedSend(sock, payload, debug)
+def init_server():
+    global sock, debug
+    progname = "Server"
+    switchesVarDefaults = (
+        (('-l', '--listenPort') ,'listenPort', 50001),
+        (('-d', '--debug'), "debug", False),
+        (('-?', '--usage'), "usage", False))
+    paramMap = params.parseParams(switchesVarDefaults)
+    debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+    if paramMap['usage']:
+        params.usage()
+
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
+    bindAddr = ("127.0.0.1", listenPort)
+    lsock.bind(bindAddr)
+    lsock.listen(5)
+    print("listening on:", bindAddr)
+    sock, addr = lsock.accept()
+    print("connection rec'd from", addr)
+    if not os.path.exists("%s/server_files" % os.getcwd()):
+        os.makedirs("server_files")
+        print("Created server_files Directory.")
+        
+if __name__ == '__main__':
+    init_server()
+    while True:
+        payload = framedReceive(sock)
+        print("Received: ", payload.decode())
+        if re.match("put\s[\w\W]+", payload.decode()):
+            trash, file = payload.decode().split(" ",1)
+            writer = open("%s/server_files/%s" % (os.getcwd(), file), "wb+")
+            framedSend(sock, "start".encode())
+            payload = framedReceive(sock,1)
+            writer.write(payload)
+            writer.close()
+            print("Done.")
+        elif payload == "quit".encode():
+            framedSend(sock, "Server Killed.".encode(), debug)
+            print("Exiting...")
+            sys.exit(0)
+        else:
+            framedSend(sock, "Received".encode(), debug)
